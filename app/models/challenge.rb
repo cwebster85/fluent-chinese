17734dddeb5921b8e9dc26e5714b7b21435ec317
Claire Lo <caudelan@gmail.com>
2022-02-22 20:55:29 +0000

    changed associations to solve issue on heroku

--- app/models/challenge.rb
+++ app/models/challenge.rb
@@ -1,5 +1,5 @@
 class Challenge < ApplicationRecord
-  belongs_to :sessions
+  has_many :sessions
   has_many :exercises
 
   validates :name, :description, :date, presence: true
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ApplicationRecord
   has_many :sessions
+  has_many :challenges, through: :sessions
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
