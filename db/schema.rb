74e35e60a8348b7b8d21df056625fbcfa2b81941
Keon White <keon-1@hotmail.co.uk>
2022-02-26 12:39:55 +0000

    Added active feature to navbar

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,6 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
-ActiveRecord::Schema.define(version: 2022_02_22_205713) do
-
 ActiveRecord::Schema.define(version: 2022_02_25_230104) do
 
   # These are extensions that must be enabled in order to support this database
